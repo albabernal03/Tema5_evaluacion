be9649636d953921619d8781accf5f329d9dfb1f
albabernal03 <ALBABERO@MYUAX.COM>
2022-12-19 19:13:33 +0100

    realizamos el primer punto de la tarea

--- Ejercicio_2/contador.py
+++ Ejercicio_2/contador.py
@@ -3,3 +3,7 @@ import sys
 fichero= open('contador.txt','a') #la 'a' es para añadir al final del fichero
 fichero.seek(0) #nos posicionamos al principio del fichero
 contenido= fichero.read() #leemos el contenido del fichero
+
+if len(contenido)==0:
+    contenido='0' #si el fichero esta vacio, le asignamos el valor 0
+    fichero.write(contenido) #escribimos el contenido en el fichero
