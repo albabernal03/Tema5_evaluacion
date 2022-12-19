fe7b3c6eb7cd2fd640b407e6ceb6732955bf6654
albabernal03 <ALBABERO@MYUAX.COM>
2022-12-19 19:46:36 +0100

    definimos la clase Personaje

--- Ejercicio_3/gestor.py
+++ Ejercicio_3/gestor.py
@@ -2,4 +2,14 @@ from io import open
 import pickle # importamos la libreria pickle que nos permite serializar objetos en ficheros es decir guardarlos en ficheros
 
 class Personaje:
-    
+
+    def __init__(self, nombre, vida, ataque, defensa, alcance):
+        self.nombre = nombre
+        self.vida = vida
+        self.ataque = ataque
+        self.defensa = defensa
+        self.alcance = alcance
+
+    def __str__(self):
+        return '{} => Vida: {} Ataque: {} Defensa: {} Alcance: {}'.format(self.nombre, self.vida, self.ataque, self.defensa, self.alcance)
+        
