2cebe85043e354f34a1b3a3608eb7c58d1ec9c4c
albabernal03 <ALBABERO@MYUAX.COM>
2022-12-19 20:16:00 +0100

    cambios

--- Ejercicio_4/reloj.py
+++ Ejercicio_4/reloj.py
@@ -2,7 +2,12 @@ import datetime
 import time 
 import os
 
-while True:
-    os.system('clear') # Limpia la pantalla en este caso al ser mac utilizo el comando clear
-    dt= datetime.datetime.now() # Obtenemos la fecha y hora actual
-    print('{}:{}:{}'.format(dt.hour,dt.minute,dt.second)) # Imprimimos la hora
+def main():
+    while True:
+        os.system('clear') # Limpia la pantalla en este caso al ser mac utilizo el comando clear
+        dt= datetime.datetime.now() # Obtenemos la fecha y hora actual
+        print('{}:{}:{}'.format(dt.hour,dt.minute,dt.second)) # Imprimimos la hora
+        time.sleep(1) # Esperamos 1 segundo
+
+if __name__ == '__main__':
+    main()
