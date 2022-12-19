12f4db7461fc799b56b653296b00a77393506ce0
albabernal03 <ALBABERO@MYUAX.COM>
2022-12-19 20:19:12 +0100

    probando

--- Ejercicio_2/contador.py
+++ Ejercicio_2/contador.py
@@ -1,26 +1,28 @@
 import sys 
 
-fichero= open('contador.txt','a+') #la 'a+' es para añadir y leer
-fichero.seek(0) #nos posicionamos al principio del fichero
-contenido= fichero.readline() #leemos el contenido del fichero
+def main():
+    
+    fichero= open('contador.txt','a+') #la 'a+' es para añadir y leer
+    fichero.seek(0) #nos posicionamos al principio del fichero
+    contenido= fichero.readline() #leemos el contenido del fichero
 
-if len(contenido)==0:
-    contenido='0' #si el fichero esta vacio, le asignamos el valor 0
-    fichero.write(contenido) #escribimos el contenido en el fichero
-fichero.close() #cerramos el fichero
-try:
-
-    contador= int(contenido) #convertimos el contenido a entero
-    if len(sys.argv)==2: 
-        if sys.argv[1]=='inc':
-            contador+=1
-        elif sys.argv[1]=='dec':
-            contador-=1
-    print(contador)
-    fichero= open('contador.txt','w') #la 'w' es para sobreescribir el fichero
-    fichero.write(str(contador)) #escribimos el contenido en el fichero
+    if len(contenido)==0:
+        contenido='0' #si el fichero esta vacio, le asignamos el valor 0
+        fichero.write(contenido) #escribimos el contenido en el fichero
     fichero.close() #cerramos el fichero
-except ValueError:
-    print('Error: Fichero corrupto')
+    try:
+
+        contador= int(contenido) #convertimos el contenido a entero
+        if len(sys.argv)==2: 
+            if sys.argv[1]=='inc':
+                contador+=1
+            elif sys.argv[1]=='dec':
+                contador-=1
+        print(contador)
+        fichero= open('contador.txt','w') #la 'w' es para sobreescribir el fichero
+        fichero.write(str(contador)) #escribimos el contenido en el fichero
+        fichero.close() #cerramos el fichero
+    except ValueError:
+        print('Error: Fichero corrupto')
 
 
