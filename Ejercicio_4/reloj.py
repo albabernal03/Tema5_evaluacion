4621bceb869d44af7cde0291c17144dcbd25cc65
albabernal03 <ALBABERO@MYUAX.COM>
2022-12-19 20:12:29 +0100

    creamos un bucle

--- Ejercicio_4/reloj.py
+++ Ejercicio_4/reloj.py
@@ -1,3 +1,8 @@
 import datetime
 import time 
 import os
+
+while True:
+    os.system('clear') # Limpia la pantalla en este caso al ser mac utilizo el comando clear
+    dt= datetime.datetime.now() # Obtenemos la fecha y hora actual
+    print('{}:{}:{}'.format(dt.hour,dt.minute,dt.second)) # Imprimimos la hora
