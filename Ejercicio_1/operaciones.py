0a87631d81066477fb217183c1aa713a83f29412
albabernal03 <ALBABERO@MYUAX.COM>
2022-12-19 18:55:16 +0100

    resolvemos el primer ejercicio

--- Ejercicio_1/operaciones.py
+++ Ejercicio_1/operaciones.py
@@ -1,3 +1,38 @@
 def suma(a, b):
-    return a + b
+    try:
+        a = int(a) or float(a)
+        b = int(b) or float(b)
+        return a + b
+    except ValueError: 
+        print("Error: Solo se permiten numeros")
+
+
+def resta(a, b):
+    try:
+        a = int(a) or float(a)
+        b = int(b) or float(b)
+        return a - b
+    except ValueError:
+        print("Error: Solo se permiten numeros")
+
+def producto(a, b):
+    try:
+        a = int(a) or float(a)
+        b = int(b) or float(b)
+        return a * b
+    except ValueError:
+        print("Error: Solo se permiten numeros")
+
+
+def division(a, b):
+    try:
+        a = int(a) or float(a)
+        b = int(b) or float(b) and b != 0
+        return a / b
+    except ValueError:
+        print("Error: Solo se permiten numeros")
+    except ZeroDivisionError:
+        print("Error: No se puede dividir entre cero")
+
+        
 
