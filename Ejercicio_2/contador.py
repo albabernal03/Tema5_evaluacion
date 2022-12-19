9d6d781e83a9d930be0a786ef48fc60c1eed6ec5
albabernal03 <ALBABERO@MYUAX.COM>
2022-12-19 19:20:37 +0100

    probando

--- Ejercicio_2/contador.py
+++ Ejercicio_2/contador.py
@@ -7,3 +7,20 @@ contenido= fichero.read() #leemos el contenido del fichero
 if len(contenido)==0:
     contenido='0' #si el fichero esta vacio, le asignamos el valor 0
     fichero.write(contenido) #escribimos el contenido en el fichero
+fichero.close() #cerramos el fichero
+
+try:
+    contador= int(contenido) #convertimos el contenido a entero
+    if len(sys.argv)==2: 
+        if sys.argv[1]=='inc':
+            contador+=1
+        elif sys.argv[1]=='dec':
+            contador-=1
+    print(contador)
+    fichero= open('contador.txt','w') #la 'w' es para sobreescribir el fichero
+    fichero.write(str(contador)) #escribimos el contenido en el fichero
+    fichero.close() #cerramos el fichero
+except ValueError:
+    print('Error: Fichero corrupto')
+
+
