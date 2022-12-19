6377553dab5014076c50b1947011284c6ed58caa
albabernal03 <ALBABERO@MYUAX.COM>
2022-12-19 19:25:45 +0100

    cambios
    cam

--- Ejercicio_2/contador.py
+++ Ejercicio_2/contador.py
@@ -1,8 +1,8 @@
 import sys 
 
-fichero= open('contador.txt','a') #la 'a' es para añadir al final del fichero
+fichero= open('contador.txt','a+') #la 'a+' es para añadir y leer
 fichero.seek(0) #nos posicionamos al principio del fichero
-contenido= fichero.read() #leemos el contenido del fichero
+contenido= fichero.readline() #leemos el contenido del fichero
 
 if len(contenido)==0:
     contenido='0' #si el fichero esta vacio, le asignamos el valor 0
