37739614b33e9a77328ce991d35dbe0f1c1581ef
albabernal03 <ALBABERO@MYUAX.COM>
2022-12-19 19:02:44 +0100

    inicializamos

--- Ejercicio_1/operaciones.py
+++ Ejercicio_1/operaciones.py
@@ -34,5 +34,11 @@ def division(a, b):
     except ZeroDivisionError:
         print("Error: No se puede dividir entre cero")
 
-        
 
+#probamos
+
+a,b,c,d=(10,5,0,'hola')
+print('{} + {} = {}'.format(a,b,suma(a,b)))
+print('{} - {} = {}'.format(b,d,resta(b,d)))
+print('{} * {} = {}'.format(b,b,producto(b,b)))
+print('{} / {} = {}'.format(a,c,division(a,c)))
---
+++ Ejercicio_1/run.py
@@ -0,0 +1,13 @@
+from operaciones import *
+
+if __name__ == '__main__':
+    a,b,c,d=(10,5,0,'hola')
+    print('{} + {} = {}'.format(a,b,suma(a,b)))
+    print('{} - {} = {}'.format(b,d,resta(b,d)))
+    print('{} * {} = {}'.format(b,b,producto(b,b)))
+    print('{} / {} = {}'.format(a,c,division(a,c)))
+
+
+
+
+  
