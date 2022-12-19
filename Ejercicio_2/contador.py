c610ebc1320400bbb3f18e428b6b5d8739404f9b
albabernal03 <ALBABERO@MYUAX.COM>
2022-12-19 19:11:46 +0100

    leemos el fichero

--- Ejercicio_2/contador.py
+++ Ejercicio_2/contador.py
@@ -2,3 +2,4 @@ import sys
 
 fichero= open('contador.txt','a') #la 'a' es para añadir al final del fichero
 fichero.seek(0) #nos posicionamos al principio del fichero
+contenido= fichero.read() #leemos el contenido del fichero
